79c8efea5a552114ba4a362b9c29e0a272672288
just-hoshinon <mwadman2018@gmail.com>
2022-06-05 18:43:19 +0300

    making linter not lint files with syntax errors

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import {Tokenizer} from "./tokenizer.js";
 import fs from "fs"
-
-String.prototype.matches = function (regexp) {
-  const matches = this.match(regexp);
-  if (matches == null) return false;
-  return matches[0] === String(this);
-}
+import vm from "vm";
 
 const fileName = (process.argv[2] && process.argv[2] !== "--fullLog") ? process.argv[2] : "test.js";
 const fullLog = process.argv.includes("--fullLog");
 
+const file = fs.readFileSync(`./${fileName}`, 'utf8');
 
-const file = fs.readFileSync(`./${fileName}`).toString();
+try {
+  new vm.Script(file);
+} catch (e) {
+  console.log("Before using linter you should fix syntax errors:");
+  throw e;
+}
+
 const tokenizer = new Tokenizer(file);
 
 tokenizer.tokenize();
