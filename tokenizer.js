ff9155d16a03e86ed976f26145f566b977ee40cd
just-hoshinon <mwadman2018@gmail.com>
2022-06-05 21:45:57 +0300

    updating tokenizer regulars and optimizing code

--- tokenizer.js
+++ tokenizer.js
@@ -1,6 +1,6 @@
 import {Token} from "./token.js";
 
-const keywordRegex = /^(await|break|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|false|finally|for|function|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|return|super|switch|static|this|throw|try|true|typeof|var|void|while|with|yield)/
+const keywordRegex = /^(await|break|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|false|finally|for|function|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|return|super|switch|static|this|throw|try|true|typeof|var|void|while|with|yield)(?![A-Za-z0-9$_])/
 const filteredTypes = [
   "end-of-line", "whitespace", "semicolon", "undefined"
 ];
@@ -17,7 +17,7 @@ const tokenTypes = new Map([
   ["whitespace", /^ +/],
   ["assignment-operator", /^(=(?![=>])|(\+|-|\*|\/|%|\*\*|\?\?|\|\||&&|&|\||\^|<<|>>|>>>)=)/],
   ["logical-operator", /^(\|\||&&|!)(?!=)/],
-  ["comparing-operator", /^((?<!=)[<>](?![=<>])|<=|>=|===|!=|!==)/],
+  ["comparing-operator", /^((?<!=)[<>](?![=<>])|==(?!=)|<=|>=|===|!=|!==)/],
   ["nullish-operator", /^\?\?(?!=)/],
   ["opening-brace", /^{/],
   ["closing-brace", /^}/],
@@ -33,16 +33,14 @@ const tokenTypes = new Map([
   ["end-of-line", /^(\r\n|\n)/],
   ["number", /^\d+/],
   ["keyword", keywordRegex],
-  ["variable", /^[a-zA-Z_$][a-zA-Z_$0-9]*/]
-])
+  ["identifier", /^[a-zA-Z_$][a-zA-Z_$0-9]*/]
+]);
 
 export class Tokenizer {
   text;
   checkingText;
   currentRow = 1;
   currentCol = 1;
-  currentSymbolIndex = 0;
-  currentSymbol;
   currentToken;
   filteredTokens = [];
   allTokens = [];
@@ -53,9 +51,9 @@ export class Tokenizer {
   }
 
   tokenize(){
-    this.currentSymbol = this.text[this.currentSymbolIndex];
-    while (this.currentSymbol !== null && this.currentSymbol !== undefined) {
+    while (this.checkingText !== "") {
       this.createToken();
+      this.currentToken.index = this.allTokens.length + 1;
       this.allTokens.push(this.currentToken);
       if (!filteredTypes.includes(this.currentToken.type)) this.filteredTokens.push(this.currentToken);
       this.next(this.currentToken.value.length);
@@ -99,11 +97,8 @@ export class Tokenizer {
   }
 
   next(amount = 1) {
-    this.currentSymbolIndex += amount;
-    this.currentSymbol = this.text[this.currentSymbolIndex];
     this.updateRowAndCol(amount);
     this.checkingText = this.checkingText.slice(amount);
-    return this.currentSymbol;
   }
 
   log(fullLog = false) {
