c9f2315d0b88c4774c898ba17bc52f5a5033be1f
just-hoshinon <mwadman2018@gmail.com>
2022-06-17 19:13:36 +0300

    adding Symbols enum

---
+++ sybmols.js
@@ -0,0 +1,13 @@
+export const Symbols = {
+  SEMICOLON: ';',
+  NEW_LINE: '\n',
+  DOT: '.',
+  OPENING_EMPHASISE: '(',
+  CLOSING_EMPHASISE: ')',
+  OPENING_BRACE: '{',
+  CLOSING_BRACE: '}',
+  OPENING_BRACKET: '[',
+  CLOSING_BRACKET: ']',
+  SPACE: ' ',
+  COMMA: ',',
+}
