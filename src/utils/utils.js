744c389b7857848a23567bc21511dae57f732459
just-hoshinon <mwadman2018@gmail.com>
2022-06-28 00:10:07 +0300

    simplifying utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -19,11 +19,14 @@ export class Utils {
   static findClosingBracket(opening, tokens) {
     let counter = 1;
     const array = tokens.slice(tokens.indexOf(opening) + 1);
+    const closingBracket = BRACKETS_MAP.get(opening.value);
 
     let closing;
     for (closing of array) {
-      if (closing.value === opening.value) counter++;
-      if (closing.value === BRACKETS_MAP.get(opening.value)) counter--;
+      const isOpening = closing.is(opening.value);
+      const isClosing = closing.is(closingBracket);
+      counter += isOpening - isClosing;
+
       if (counter === 0) break;
     }
 
@@ -31,7 +34,7 @@ export class Utils {
   }
 
   static joinTokens(filteredTokens, allTokens, key) {
-    const copyArray = Array.from(filteredTokens);
+    const copyArray = [...filteredTokens];
     for (const token of allTokens) {
       if (token.isType(key)) {
         if (filteredTokens[0].index > token.index) continue;
@@ -43,10 +46,10 @@ export class Utils {
   }
 
   static findIndex(filteredTokens, index) {
-    for (const token of filteredTokens) {
-      if (token.index > index) return filteredTokens.indexOf(token);
-    }
-    return filteredTokens.length;
+    const resultIndex = filteredTokens.findIndex((token) => token.index > index);
+    return resultIndex === -1 ?
+      filteredTokens.length :
+      resultIndex;
   }
 
   static plane(parts) {
